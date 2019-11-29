0931f41e7f2ff4c0052847ffcf6420490a55d01d
frontendkor <frontendkor@gmail.com>
2019-11-29 14:51:58 +0300

    Updates

---
+++ precache-manifest.6e7ce7203cf41d7f79581d9db412fb4e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d613082ae90179d79bb6cb1b60cf1480",
+    "url": "/Haltrade2018/static/media/baner-img.d613082a.jpg"
+  },
+  {
+    "revision": "64647df0e7ab715a7069",
+    "url": "/Haltrade2018/static/css/main.86e5728c.chunk.css"
+  },
+  {
+    "revision": "873dd1a4c6908aaf96c0",
+    "url": "/Haltrade2018/static/js/1.873dd1a4.chunk.js"
+  },
+  {
+    "revision": "e3399c9e8e0838fc69d6",
+    "url": "/Haltrade2018/static/js/runtime~main.e3399c9e.js"
+  },
+  {
+    "revision": "8eb7a30726023457dc22e68368345f0b",
+    "url": "/Haltrade2018/static/media/bgOrder.8eb7a307.png"
+  },
+  {
+    "revision": "0526e330121260520d121308dbbfad2d",
+    "url": "/Haltrade2018/static/media/ThanksImg.0526e330.png"
+  },
+  {
+    "revision": "3430f768c0888db48a699f5f571cb02c",
+    "url": "/Haltrade2018/static/media/logo.3430f768.png"
+  },
+  {
+    "revision": "64647df0e7ab715a7069",
+    "url": "/Haltrade2018/static/js/main.64647df0.chunk.js"
+  },
+  {
+    "revision": "7f55992bd15cd43221cb0c13d221c5d1",
+    "url": "/Haltrade2018/static/media/description-img.7f55992b.jpg"
+  },
+  {
+    "revision": "197afe81ff6a2e30e21a3ec064892b8d",
+    "url": "/Haltrade2018/static/media/chiken.197afe81.png"
+  },
+  {
+    "revision": "326fb37383667ffcfb1a297dbf0af9f9",
+    "url": "/Haltrade2018/static/media/filet.326fb373.png"
+  },
+  {
+    "revision": "fadea4f8f03da495c457ad0e7a095a59",
+    "url": "/Haltrade2018/static/media/ham.fadea4f8.png"
+  },
+  {
+    "revision": "eaecafef85d2071075c2b7372cf3ad43",
+    "url": "/Haltrade2018/static/media/certificate.eaecafef.jpg"
+  },
+  {
+    "revision": "24cc4a8aeb26590f898d5509b17bee43",
+    "url": "/Haltrade2018/index.html"
+  }
+];
